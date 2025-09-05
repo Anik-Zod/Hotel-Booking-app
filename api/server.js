284d8a62d1fdfaf3ea0df1d1e8d3ccb2bbb7d426
Anik Zod <anikdas169@gmail.com>
2025-09-05 16:52:53 +0600

    final

--- api/server.js
+++ api/server.js
@@ -18,8 +18,9 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "*",          // allow all origins
+    origin: [ process.env.FRONTEND_URL,process.env.ADMIN_URL],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    credentials: true,
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
