cddfda3cc830b6be713857347ed86f690fdd2ab7
Anik Zod <anikdas169@gmail.com>
2025-12-24 00:24:50 +0600

    sss

--- admin/lib/auth-client.js
+++ admin/lib/auth-client.js
@@ -3,6 +3,9 @@ import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
   baseURL: `${import.meta.env.VITE_API_URL}/auth`,
+  fetchOptions: {
+    credentials: "include", // 🔴 THIS IS THE FIX
+  },
   plugins: [
     inferAdditionalFields({
       user: {
